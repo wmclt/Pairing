25fadf44498bcb7f58015e82089b3bde37b54836
WilliamM <william.mauclet@gmail.com>
2017-10-23 17:48:47 +0200

    Now takes into account solo history to decide who should go solo.

--- pairing.py
+++ pairing.py
@@ -33,10 +33,7 @@ def main():
     good_pairing_found = False
     args = sys.argv[1:]
     while not good_pairing_found:
-        devs = ['Luc','Gilles','Bert','William','Johan','Wouter','Michel']
-        pairs = []
-        leads = []
-        _process_args(args, devs, pairs, leads)
+        pairs = _create_pairing(args)
 
         print("")
         for pair in pairs:
@@ -49,11 +46,13 @@ def main():
 
     _increment_cumul_chosen_pairs_in_history(pairs)
 
-def _process_args(args, devs, pairs, leads):
+def _create_pairing(args):
+    devs = ['Luc','Gilles','Bert','William','Johan','Wouter','Michel']
+    leads, pairs = [], []
     options_with_values = _group_values_with_their_options(args)
 
     if "-help" in options_with_values:
-        _print_help()
+        print_help()
         sys.exit(2)
 
     if "-absent" in options_with_values:
@@ -78,6 +77,8 @@ def _process_args(args, devs, pairs, leads):
                 devs.remove(dev)
 
     _form_pairs(devs, pairs, leads)
+    
+    return pairs
 
 def _group_values_with_their_options(args):
     options_with_values = {}
@@ -105,14 +106,42 @@ def _create_pairs_with_leads(devs, pairs, leads, history):
         pairs.append(_create_pair(lead, other))
 
 def _create_pairs_without_leads(devs, pairs, history):
-    # TODO first check if uneven, do the solo, then pairs
+    if len(devs)%2 == 1:
+        dev = _pop_random_least_alone(devs, history)        
+        pairs.append((dev,))
+
     while len(devs) > 0:
 	    one = _pop_random(devs)
-	    if len(devs) is 0: # last remaining dev
-		    pairs.append((one))
-	    else:
-		    two = _pop_random_least_paired_with(one, devs, history)
-		    pairs.append(_create_pair(one,two))
+	    two = _pop_random_least_paired_with(one, devs, history)
+	    pairs.append(_create_pair(one,two))
+
+def _pop_random_least_alone(devs, history):
+    solo_cumuls = []
+    for dev in devs:
+        solo_cumuls.append(_get_solo_history(dev, history))
+    
+    minimum = 99999
+    min_soloists = []
+    for cumul in solo_cumuls:
+        if cumul[1] < minimum:
+            min_soloists = [cumul[0]]
+            minimum = cumul[1]
+        elif cumul[1] == minimum:
+            min_soloists.append(cumul[0])
+        else:
+            pass
+
+    chosen_dev = choice(min_soloists)
+    devs.remove(chosen_dev)
+    return chosen_dev
+
+def _pop_random(people):
+	return people.pop(people.index(choice(people)))
+
+def _get_solo_history(dev, history):
+    for pair in history:
+        if dev in pair and len(pair) == 1:
+            return (dev, history[pair])
 
 def _pop_random_least_paired_with(dev, devs, history):
     pairs_with_dev = _get_pairs_with_dev_and_unchosen_partner(dev, devs, history)
@@ -194,12 +223,12 @@ def _create_pair(dev_1, dev_2):
     return tuple(sorted([dev_1, dev_2]))
 
 def _pair_str_repr(pair):
-    if type(pair) == str:
-        return pair
+    if len(pair) == 1:
+        return pair[0]
     else:
         return "{}{}{}".format(pair[0], PAIR_SPLIT_SIGN, pair[1])
 
-def _print_help():
+def print_help():
     help_str = """
     COMMAND:
         ./pairing.py
@@ -215,9 +244,6 @@ def _print_help():
         $ ./pairing.py -absent Wouter -lead Bert Michel -staying Gilles:Luc
     """
     print(help_str)
-
-def _pop_random(people):
-	return people.pop(people.index(choice(people)))
 
 def _increment_cumul_chosen_pairs_in_history(pairs):
     history = _fetch_history()
